a76efa8f9dff0e8261759f2ea8896e54395255bc
twc0326 <101781213+twc0326@users.noreply.github.com>
2023-05-04 17:25:21 +0800

    Add files via upload

---
+++ EmpApp.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'lab'
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about", methods=['POST'])
+def about():
+    return render_template('index.html')
+
+
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    gender = request.form['gender']
+    add = request.form['add']
+
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    try:
+
+        cursor.execute(insert_sql, (name, email, phone, gender, add))
+        db_conn.commit()
+
+       
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "lab-db.cnx6udekt8nx.us-east-1.rds.amazonaws.com"
+customuser = "main"
+custompass = "lab-password"
+customdb = "lab"
+customregion = "us-east-1"
+
