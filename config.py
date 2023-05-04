4b9556d5b2c87203a5f0d21ff6aa0ee78fc78354
twc0326 <101781213+twc0326@users.noreply.github.com>
2023-05-04 23:00:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "lab-db.css3kgpgmuh7.us-east-1.rds.amazonaws.com"
+customhost = "lab-db1.css3kgpgmuh7.us-east-1.rds.amazonaws.com"
 customuser = "main"
 custompass = "lab-password"
-customdb = "lab"
+customdb = "lab1"
 customregion = "us-east-1"
 
